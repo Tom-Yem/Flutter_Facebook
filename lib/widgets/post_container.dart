c29f205860aee22166c93dcb4c8ccefd16dc0972
Tom <Tomy51451@gmail.com>
2021-08-31 13:27:18 +0200

    :sparkles: NEW: Add post feed

---
+++ lib/widgets/post_container.dart
@@ -0,0 +1,206 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_facebook_responsive_ui/config/palette.dart';
+import 'package:flutter_facebook_responsive_ui/models/models.dart';
+import 'package:flutter_facebook_responsive_ui/widgets/profile_avatar.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+
+class PostContainer extends StatelessWidget {
+  const PostContainer({Key? key, required this.post}) : super(key: key);
+
+  final Post post;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 5.0),
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      color: Colors.white,
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 12.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                _PostHeader(post: post),
+                const SizedBox(
+                  height: 4.0,
+                ),
+                Text(post.caption),
+                post.imageUrl != null
+                    ? const SizedBox.shrink()
+                    : const SizedBox(
+                        height: 6.0,
+                      )
+              ],
+            ),
+          ),
+          post.imageUrl != null
+              ? Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8.0),
+                  child: CachedNetworkImage(imageUrl: post.imageUrl!),
+                )
+              : const SizedBox.shrink(),
+          Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 12.0),
+              child: _PostStats(post: post))
+        ],
+      ),
+    );
+  }
+}
+
+class _PostHeader extends StatelessWidget {
+  const _PostHeader({Key? key, required this.post}) : super(key: key);
+
+  final Post post;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        ProfileAvatar(imageUrl: post.user.imageUrl),
+        const SizedBox(width: 8.0),
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                post.user.name,
+                style: TextStyle(fontWeight: FontWeight.w600),
+              ),
+              Row(
+                children: [
+                  Text(
+                    "${post.timeAgo} • ",
+                    style: TextStyle(color: Colors.grey[600], fontSize: 12.0),
+                  ),
+                  Icon(
+                    Icons.public,
+                    color: Colors.grey[600],
+                    size: 12.0,
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+        IconButton(
+          onPressed: () => print("more"),
+          icon: Icon(Icons.more_horiz),
+        ),
+      ],
+    );
+  }
+}
+
+class _PostStats extends StatelessWidget {
+  const _PostStats({Key? key, required this.post}) : super(key: key);
+
+  final Post post;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          children: [
+            Container(
+              padding: EdgeInsets.all(4.0),
+              decoration: BoxDecoration(
+                  color: Palette.facebookBlue, shape: BoxShape.circle),
+              child: Icon(
+                Icons.thumb_up,
+                size: 10.0,
+                color: Colors.white,
+              ),
+            ),
+            const SizedBox(
+              width: 4.0,
+            ),
+            Text(
+              "${post.likes}",
+              style: TextStyle(color: Colors.grey[600]),
+            ),
+            const Spacer(),
+            Text(
+              "${post.comments} Comments",
+              style: TextStyle(color: Colors.grey[600]),
+            ),
+            const SizedBox(
+              width: 8.0,
+            ),
+            Text(
+              "${post.shares} Shares",
+              style: TextStyle(color: Colors.grey[600]),
+            )
+          ],
+        ),
+        const Divider(),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            _PostButton(
+              icon: Icon(
+                MdiIcons.thumbUpOutline,
+                size: 20,
+                color: Colors.grey[600],
+              ),
+              label: "Like",
+              onTap: () => print("like"),
+            ),
+            _PostButton(
+              icon: Icon(
+                MdiIcons.commentOutline,
+                size: 20,
+                color: Colors.grey[600],
+              ),
+              label: "Comment",
+              onTap: () => print("Comment"),
+            ),
+            _PostButton(
+              icon: Icon(
+                MdiIcons.shareOutline,
+                size: 25,
+                color: Colors.grey[600],
+              ),
+              label: "Share",
+              onTap: () => print("Share"),
+            )
+          ],
+        )
+      ],
+    );
+  }
+}
+
+class _PostButton extends StatelessWidget {
+  const _PostButton(
+      {Key? key, required this.label, required this.icon, required this.onTap})
+      : super(key: key);
+
+  final String label;
+  final Icon icon;
+  final Function() onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.white,
+      child: InkWell(
+        onTap: onTap,
+        child: Container(
+          height: 25.0,
+          child: Row(children: [
+            icon,
+            const SizedBox(
+              width: 4.0,
+            ),
+            Text(label)
+          ]),
+        ),
+      ),
+    );
+  }
+}
