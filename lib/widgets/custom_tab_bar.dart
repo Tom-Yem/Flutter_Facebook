2aab2bf311dce5be8c2e4a6e95404efd077c4a2c
Tom <Tomy51451@gmail.com>
2021-09-01 11:53:55 +0200

    :sparkles: NEW: Add multiple screens & navigation

---
+++ lib/widgets/custom_tab_bar.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_facebook_responsive_ui/config/palette.dart';
+
+class CustomTabBar extends StatelessWidget {
+  const CustomTabBar(
+      {Key? key,
+      required this.icons,
+      required this.selectedIndex,
+      required this.onTap})
+      : super(key: key);
+
+  final List<IconData> icons;
+  final int selectedIndex;
+  final Function(int) onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return TabBar(
+      indicator: BoxDecoration(
+        border: Border(
+          top: BorderSide(color: Palette.facebookBlue, width: 3.0),
+        ),
+      ),
+      tabs: icons
+          .asMap()
+          .map(
+            (i, icon) => MapEntry(
+              i,
+              Tab(
+                icon: Icon(
+                  icon,
+                  color: i == selectedIndex
+                      ? Palette.facebookBlue
+                      : Colors.black45,
+                  size: 30.0,
+                ),
+              ),
+            ),
+          )
+          .values
+          .toList(),
+      onTap: onTap,
+    );
+  }
+}
