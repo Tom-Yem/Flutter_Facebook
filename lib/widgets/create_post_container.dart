f38843870f01086c998354063d96d8728b16b8bb
Tom <Tomy51451@gmail.com>
2021-09-02 09:48:57 +0200

    :lipstick: UI: update create-post & rooms
    Accroding to design

--- lib/widgets/create_post_container.dart
+++ lib/widgets/create_post_container.dart
@@ -11,75 +11,86 @@ class CreatePostContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.fromLTRB(12, 8, 12, 0),
-      color: Colors.white,
-      child: Column(
-        children: [
-          Row(
-            children: [
-              ProfileAvatar(imageUrl: currentUser.imageUrl),
-              const SizedBox(
-                width: 8.0,
-              ),
-              Expanded(
-                child: TextField(
-                  decoration: InputDecoration.collapsed(
-                    hintText: "What's on your mind?",
-                  ),
-                ),
-              )
-            ],
-          ),
-          const Divider(
-            height: 10.0,
-            thickness: 0.5,
-          ),
-          Container(
-            height: 40.0,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    final bool isDesktop = Responsive.isDesktop(context);
+
+    return Card(
+      elevation: isDesktop ? 1.0 : 0.0,
+      shape: isDesktop
+          ? RoundedRectangleBorder(borderRadius: BorderRadius.circular(10))
+          : null,
+      child: Container(
+        //adding margin makes container to shrink horizontally so that the card's circular
+        //borders & horizontal drop shadow can be seen
+        margin: EdgeInsets.symmetric(horizontal: isDesktop ? 5.0 : 0.0),
+        padding: const EdgeInsets.fromLTRB(12, 8, 12, 0),
+        color: Colors.white,
+        child: Column(
+          children: [
+            Row(
               children: [
-                TextButton.icon(
-                  onPressed: () => print("live"),
-                  icon: Icon(
-                    Icons.videocam,
-                    color: Colors.red,
-                  ),
-                  label: Text(
-                    "Live",
-                    style: TextStyle(color: Colors.black),
-                  ),
-                ),
-                const VerticalDivider(
+                ProfileAvatar(imageUrl: currentUser.imageUrl),
+                const SizedBox(
                   width: 8.0,
                 ),
-                TextButton.icon(
-                  onPressed: () => print("Photo"),
-                  icon: Icon(Icons.photo_library, color: Colors.green),
-                  label: Text(
-                    "Photo",
-                    style: TextStyle(color: Colors.black),
-                  ),
-                ),
-                const VerticalDivider(
-                  width: 8.0,
-                ),
-                TextButton.icon(
-                  onPressed: () => print("Room"),
-                  icon: Icon(
-                    Icons.video_call,
-                    color: Colors.purpleAccent,
-                  ),
-                  label: Text(
-                    "Room",
-                    style: TextStyle(color: Colors.black),
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration.collapsed(
+                      hintText: "What's on your mind?",
+                    ),
                   ),
                 )
               ],
             ),
-          )
-        ],
+            const Divider(
+              height: 10.0,
+              thickness: 0.5,
+            ),
+            Container(
+              height: 40.0,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  TextButton.icon(
+                    onPressed: () => print("live"),
+                    icon: Icon(
+                      Icons.videocam,
+                      color: Colors.red,
+                    ),
+                    label: Text(
+                      "Live",
+                      style: TextStyle(color: Colors.black),
+                    ),
+                  ),
+                  const VerticalDivider(
+                    width: 8.0,
+                  ),
+                  TextButton.icon(
+                    onPressed: () => print("Photo"),
+                    icon: Icon(Icons.photo_library, color: Colors.green),
+                    label: Text(
+                      "Photo",
+                      style: TextStyle(color: Colors.black),
+                    ),
+                  ),
+                  const VerticalDivider(
+                    width: 8.0,
+                  ),
+                  TextButton.icon(
+                    onPressed: () => print("Room"),
+                    icon: Icon(
+                      Icons.video_call,
+                      color: Colors.purpleAccent,
+                    ),
+                    label: Text(
+                      "Room",
+                      style: TextStyle(color: Colors.black),
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
--- lib/widgets/rooms.dart
+++ lib/widgets/rooms.dart
@@ -10,29 +10,40 @@ class Rooms extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 60,
-      color: Colors.white,
-      child: ListView.builder(
-          padding: EdgeInsets.symmetric(vertical: 10, horizontal: 4),
-          scrollDirection: Axis.horizontal,
-          itemCount: 1 + onlineUsers.length,
-          itemBuilder: (context, index) {
-            if (index == 0)
-              return Padding(
-                padding: EdgeInsets.symmetric(horizontal: 8),
-                child: _CreateRoomButton(),
-              );
-            final User user = onlineUsers[index - 1];
+    final bool isDesktop = Responsive.isDesktop(context);
 
-            return Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8.0),
-              child: ProfileAvatar(
-                imageUrl: user.imageUrl,
-                isActive: true,
-              ),
-            );
-          }),
+    return Card(
+      elevation: isDesktop ? 1.0 : 0.0,
+      shape: isDesktop
+          ? RoundedRectangleBorder(borderRadius: BorderRadius.circular(10))
+          : null,
+      child: Container(
+        //adding margin makes container to shrink horizontally so that the card's circular
+        //borders & horizontal drop shadow can be seen
+        margin: EdgeInsets.symmetric(horizontal: isDesktop ? 5.0 : 0.0),
+        height: 60,
+        color: Colors.white,
+        child: ListView.builder(
+            padding: EdgeInsets.symmetric(vertical: 10, horizontal: 4),
+            scrollDirection: Axis.horizontal,
+            itemCount: 1 + onlineUsers.length,
+            itemBuilder: (context, index) {
+              if (index == 0)
+                return Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 8),
+                  child: _CreateRoomButton(),
+                );
+              final User user = onlineUsers[index - 1];
+
+              return Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: ProfileAvatar(
+                  imageUrl: user.imageUrl,
+                  isActive: true,
+                ),
+              );
+            }),
+      ),
     );
   }
 }
